78e3ad7b3f8de1f1da6f568fea3f1a0084bde6ef
Anuradha Karunarathna <Anuradha Karunarathna>
2017-06-28 19:42:33 +0530

    added Book.php

---
+++ Book.php
@@ -0,0 +1,20 @@
+<?php
+class Book extends table{
+	var $title=null;
+	var $acc_no=null;
+	var $ISBN=null;
+	var $category_no=null;
+	var $publisher_name=null;
+	var $published_date=null;
+	var $price=null;
+	var $no_pages=null;
+	var $date_added=null;
+	var $status=null;
+	var $table="books";
+
+
+}
+
+
+?>
+
