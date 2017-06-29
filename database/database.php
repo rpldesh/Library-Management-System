c0543e89cd3d528bfd5d6c4f4a722082d44c0fb0
Lakshitha Rajapakse <lakshitha.15@cse.mrt.ac.lk>
2017-06-29 14:58:01 +0530

    add admin, user, database files

---
+++ database/database.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: DiniX
+ * Date: 27-Jun-17
+ * Time: 9:47 AM
+ */
+class database
+{
+    private $host;
+    private $user;
+    private $password;
+    private $dbName;
+    private $connection;
+    private $results;
+    private $numRows;
+
+    private static $dbInstance;
+
+    private function __construct()
+    {
+    }
+
+    public static function getInstance()
+    {
+        if (! self::$dbInstance){
+            self::$dbInstance = new self();
+        }
+        return self::$dbInstance;
+    }
+
+    function connect($host, $user, $password, $dbName){
+        $this->host = $host;
+        $this->user = $user;
+        $this->password = $password;
+        $this->dbName = $dbName;
+        $this->connection = mysqli_connect($this->host,$this->user,$this->password,$this->dbName);
+    }
+
+    public function doQuery($sql){
+        $this->results = mysqli_query($this->connection, $sql);
+        $this->numRows = $this->results->num_rows;
+    }
+
+    public function loadObjList(){
+        $objArray = "No Results";
+        if($this->results){
+            $objArray = mysqli_fetch_assoc($this->results);
+        }
+        return $objArray;
+    }
+}
+
+?>
---
+++ database/table.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: DiniX
+ * Date: 27-Jun-17
+ * Time: 10:05 AM
+ */
+abstract class table
+{
+    protected $id = null;
+    protected $table = null;
+
+    function __construct()
+    {
+    }
+
+
+    protected function buildQuery($task){
+        $sql = '';
+        if($task == 'load'){
+            $sql = "SELECT * from {$this->table} where id = '{$this->id}'";
+            return $sql;
+        }
+        elseif ($task == 'store'){
+            if($this->id == null){
+                $keys = "";
+                $values = "";
+                $classAttributes = get_class_vars(get_class($this));
+                $sql .= "INSERT into {$this->table} ";
+                foreach($classAttributes as $key=>$value){
+                    if ($key == "id" || $key =="table"){
+                        continue;
+                    }
+
+                    $keys .= "{$key},";
+                    $values .="'{$this->$key}',";
+                }
+                $sql .= "(".substr($keys,0,-1).") Values(".substr($values,0,-1).")";
+                echo $sql;
+                return $sql;
+            }else{
+                $classAttributes = get_class_vars(get_class($this));
+                $sql .= "UPDATE {$this->table} set";
+                foreach($classAttributes as $key=>$value){
+                    if ($key == "id" || $key =="table"){
+                        continue;
+                    }
+                    $sql .= "{$key} = '{$this->$key}',";
+                }
+                $sql = substr($sql,0,-1)." where id = {$this->id}";
+                return $sql;
+            }
+        }
+    }
+
+    function load($id){
+        $this->id = $id;
+        $dbObj = database::getInstance();
+        $sql = $this->buildQuery('load');
+        $dbObj->doQuery($sql);
+        $rows = $dbObj->loadObjList();
+        foreach ($rows as $key=> $value){
+            if($key == 'id'){
+                continue;
+            }
+            $this->$key = $value;
+        }
+    }
+
+    function store(){
+        $dbObj = database::getInstance();
+        $sql = $this->buildQuery('store');
+        $dbObj->doQuery($sql);
+    }
+
+    function bind($data){
+        foreach($data as $key=>$value){
+            $this->$key = $value;
+        }
+    }
+
+}
+?>
