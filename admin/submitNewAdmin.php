6aaa577249679a0f6ddb36bc61417fc11a453062
dinix721 <dinikasen@gmail.com>
2017-06-30 23:36:42 +0530

    submitNewAdmin.php class was modified

--- admin/submitNewAdmin.php
+++ admin/submitNewAdmin.php
@@ -38,45 +38,7 @@ if(isset($_POST['submit'])) {
         }
     }
 }
-    //$admin = new admin();
-    //$data = array("id"=>2,"fname"=>"Panther", "lname"=>"Pink");
-    //$admin->bind($data);
-    //$admin->insert($dbObj);
-    //echo $adminName." ".$adminType." ".$username." ".$password;
-
-else{
-    echo ".....";
-}
-
-/*
-$user1 = new user();
-//$user1->load($dbObj,'3');
-//echo "{$user1->fname} {$user1->lname}";
-//$data = array("id"=>2,"fname"=>"Panther", "lname"=>"Pink");
-//$user1->bind($data);
-//echo "{$user1->id} {$user1->fname} {$user1->lname}";
-//$user1->update($dbObj);
-//$user1->insert($dbObj);
-//echo "{$user1->fname} {$user1->lname}";
-//$out = $user1->featuredLoad($dbObj,"SELECT lname from users where fname = 'Scooby'");
-//foreach ($out as $key=>$value){
-//    echo $key." --- ".$value."<br />";
-//}
-if(isset($_POST['add'])) {
-$emp_name = $_POST['emp_name'];
-$emp_address = $_POST['emp_address'];
-
-$emp_salary = $_POST['emp_salary'];
-$sql = "INSERT INTO employee (emp_name,emp_address, emp_salary, join_date) VALUES('$emp_name','$emp_address',$emp_salary, NOW())";
-mysqli_select_db($connection,'test_db');
-$retval = mysqli_query( $connection,$sql );
-if(! $retval ) {
-die('Could not enter data: ');
-}
-echo "Entered data successfully\n";
-mysqli_close($connection);
-*/
-
+$dbObj->closeConnection();
 
 ?>
 
