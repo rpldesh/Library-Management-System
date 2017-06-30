2c3334e4937bdca2e27343b2d9d3e3024928c5a7
Anuradha Karunarathna <Anuradha Karunarathna>
2017-06-30 14:22:21 +0530

    created login,book_session & member classes

---
+++ member.php
@@ -0,0 +1,21 @@
+<?php
+
+
+class member extends table
+{
+   var $id=null;
+   var $member_name=null;
+   var $member_fullname=null;
+   var $member_type=null;
+   var $join_date=null;
+   var $addmission_date=null;
+   var $permanent_address=null;
+   var $currennt_address=null;
+   var $member_email=null;
+   var $contact_no=null;
+   var $member_status=null;
+   var $table="members";
+
+}
+
+?>
