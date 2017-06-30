e7ea55c2e6dd1606740e116c11aebfea63013f1c
Shalika-Madhushanki <shali.madhushanki@gmail.com>
2017-06-30 11:11:20 +0530

    created books table

--- database/createTables.php
+++ database/createTables.php
@@ -25,6 +25,32 @@ if(! $retval ) {
     die('Could not create table: ');
 }
 echo "Table admins created successfully\n";
+
+//create books table
+$sql = "CREATE TABLE IF NOT EXISTS 'books'(
+        id INT NOT NULL,
+        title VARCHAR(500) NOT NULL,
+		author VARCHAR(500) NOT NULL,
+        ISBN VARCHAR(50),
+        category_no VARCHAR(50) NOT NULL,
+        publisher_name VARCHAR(500) NOT NULL,
+        published_date DATE NOT NULL,
+        price FLOAT NOT NULL,
+        no_pages INT  NOT NULL,
+        date_added TIMESTAMP(6) NOT NULL,
+        book_type VARCHAR(100) NOT NULL,
+        status VARCHAR(50) NOT NULL,
+        primary key ())";
+$retval = mysqli_query($connection,$sql);
+if(!$retval){
+    die('Could not create table:');
+}
+echo "Table books created successfully\n";S
+
+
+
+
+
 mysqli_close($connection);
 ?>
 
