bc7a03e0798681f2ef055e3c25137057dbb9e6f1
dinix721 <dinikasen@gmail.com>
2017-07-02 00:30:40 +0530

    modified add new admin files

--- admin/submitNewAdmin.php
+++ admin/submitNewAdmin.php
@@ -11,38 +11,39 @@ include("../table.php");
 include("admin.php");
 $dbObj = database::getInstance();
 $dbObj->connect('localhost','root','','lms_db');
-
+$message = "";
 if(isset($_POST['submit'])) {
     $adminName = $_POST['adminName'];
     $adminType = $_POST['adminType'];
     $uName = $_POST['uName'];
     $pwd = $_POST['pwd'];
     $rePwd = $_POST['rePwd'];
+
     if ($pwd != $rePwd) {
-        echo "Re-entered password does not match..!!";
+        $message = "Re-entered password does not match..!!";
     }
     elseif (strlen($pwd)>64 or strlen($pwd)<8){
-        echo "Your password must contain 8-64 characters..!!";
+        $message = "Your password must contain 8-64 characters..!!";
     }else {
         $admin = new admin();
         $sql1 = "Select id FROM admins WHERE username = '{$uName}' LIMIT 1";
         $result1 = $admin->featuredLoad($dbObj, $sql1);
         if (count($result1)>0) {
-            echo "Username already exists. Please select another username..!!";
+            $message = "Username already exists. Please select another username..!!";
         }else{
             $sql2 = "Select count(*) FROM admins";
-            echo $sql2."<br />";
+            //echo $sql2."<br />";
             $result2 = $admin->featuredLoad($dbObj,$sql2);
-            echo count($result2)."<br />";
+            //echo count($result2)."<br />";
             foreach ($result2 as $key=>$value){
                 $noOfRows = $value;
             }
             $newId = $noOfRows+1;
-            echo $newId."<br />";
+            //echo $newId."<br />";
             $data = array("id"=>$newId, "admin_name" => $adminName, "admin_type" => $adminType, "username" => $uName, "pwd" => $pwd, "join_date" => time(), "admin_status" => "allowed");
             $admin->bind($data);
             $admin->insert($dbObj);
-            echo "Admin member account successfully created..!!";
+            $message = "Admin member account successfully created..!!";
             /*$sql = "Select join_date FROM admins WHERE id = 4";
             $result = $admin->featuredLoad($dbObj,$sql);
             foreach ($result as $key=>$value){
@@ -61,14 +62,11 @@ $dbObj->closeConnection();
 
 
 
-
-
 <!DOCTYPE html>
 <html>
-<!--
 <head>
-    <title>Create Admin Account</title>
-    <link rel = "stylesheet" href ="addNewAdminPageStyle.css"/>
+    <title>Add New Admin Member</title>
+    <link rel = "stylesheet" href ="AddBook.css"/>
 </head>
 <body>
 <header>
@@ -84,22 +82,21 @@ $dbObj->closeConnection();
         <div class="bgimage">
             <nav>
                 <ul>
-                    <li><a href="#">HOME</a></li>
-                    <li class="logout"><a href="#">LOGOUT</a></li>
+                    <li><a href="Administration Page.html">HOME</a></li>
+                    <li><a href="#">ADMIN PROFILE</a></li>
+                    <li class="logout"><a href="../mainpage.html">LOGOUT</a></li>
                 </ul>
             </nav>
         </div>
 </header>
-        <div class="container">
 
 
+    <form  method="POST" action="afterAddBook.php" autocomplete="off"></form>
 
+<div class = "MessageBox"><?php echo $message ?><img class="closeIcon" src="images/closebtn.png"/></div>
 
-        </div>
 </article>
 
 </body>
--->
-
 </html>
 
