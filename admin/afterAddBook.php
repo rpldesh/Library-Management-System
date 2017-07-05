e8e09e91660ed9bccb764d9754f4ffed17c0cf94
Shalika-Madhushanki <shali.madhushanki@gmail.com>
2017-07-05 11:12:34 +0530

    modified AddBook and after add book pages

--- admin/afterAddBook.php
+++ admin/afterAddBook.php
@@ -1,9 +1,69 @@
 
-<?php $message="";?>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shalika
+ * Date: 7/1/2017
+ * Time: 4:56 PM
+ */
+
+include("../database.php");
+include("../table.php");
+include("../book.php");
+$message = "";
+$dbObj = database::getInstance();
+$dbObj->connect('localhost','root','','LMS_DB');
+
+if(isset($_POST['save'])){
+    echo "in isset";
+    $title =$_POST['title'];
+    $id=$_POST['accNo'];
+    $ISBN=$_POST['isbn'];
+    $author=$_POST['AutName'];
+    $category_no=$_POST['CatNo'];
+    $publisher_name=$_POST['Pubname'];
+    $published_date=$_POST['DOP'];
+    $publisher_address=$_POST['POP'];
+    $price=$_POST['Price'];
+    $no_pages = $_POST['NoOfPg'];
+    $date_added=time();
+    $book_type=$_POST['Bktype'];
+    $book_status="available";
+    $remarks=$_POST['Remarks'];
+
+    $book=new book();
+    $sql1 = "Select id FROM books WHERE id = '{$id}' LIMIT 1";
+    $result1 = $book->featuredLoad($dbObj, $sql1);
+    if (mysqli_num_rows($result1)>0) {
+        $message = "This accession number already exists. Please enter correct accession number..!!";
+    }if($published_date>time()){
+        $message = "This is a future date.re enter the correct date..!";
+    }if($no_pages<=0){
+        $message = "No of pages cannot be negative value or 0. re enter the correct value..!";
+    }if (is_float($price+0) && ($price>0)){
+        $message = "--------------------------------";
+    }
+
+    $data = array("id"=>$id,"title"=>$title,"author"=>$author,"ISBN"=>$ISBN,"category_no"=>$category_no,
+        "publisher_name"=>$publisher_name,"published_date"=>$published_date,"publisher_address"=>$publisher_address,"price"=>$price,
+        "no_pages"=>$no_pages,"date_added"=>$date_added,"book_type"=>$book_type,"book_status"=>$book_status,"remarks"=>$remarks);
+
+    $book->bind($data);
+    $book->insert($dbObj);
+    $message= "Book added successfully..!!";
+
+    echo $id." ".$title." ".$author." ".$ISBN." ".$category_no." ".$publisher_name." ".$published_date." ".$publisher_address." ".$price." ".$no_pages." ".$date_added." ".$book_type." ".$book_status."".$remarks;
+}
+
+$dbObj->closeConnection();
+?>
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Add New Admin Member</title>
+    <title>Add New Book</title>
     <link rel = "stylesheet" href ="AddBook.css"/>
 </head>
 <body>
@@ -37,65 +97,3 @@
 
 </body>
 </html>
-
-
-<?php
-/**
- * Created by PhpStorm.
- * User: Shalika
- * Date: 7/1/2017
- * Time: 4:56 PM
- */
-
-include("../database.php");
-include("../table.php");
-include("../book.php");
-$message = "";
-$dbObj = database::getInstance();
-$dbObj->connect('localhost','root','','LMS_DB');
-
-if(isset($_POST['save'])){
-    echo "in isset";
-    $title =$_POST['title'];
-    $id=$_POST['accNo'];
-    $ISBN=$_POST['isbn'];
-    $author=$_POST['AutName'];
-    $category_no=$_POST['CatNo'];
-    $publisher_name=$_POST['Pubname'];
-    $published_date=$_POST['DOP'];
-    $publisher_address=$_POST['POP'];
-    $price=$_POST['Remarks'];
-    $no_pages = $_POST['NoOfPg'];
-    $date_added=time();
-    $book_type=$_POST['Bktype'];
-    $book_status="available";
-    $remarks=$_POST['Remarks'];
-
-    $book=new book();
-    $sql1 = "Select id FROM books WHERE id = '{$id}' LIMIT 1";
-    $result1 = $book->featuredLoad($dbObj, $sql1);
-    if (count($result1)>0) {
-        $message = "This accession number already exists. Please enter correct accession number..!!";
-    }if($published_date>time()){
-        $message = "This is a future date.re enter the correct date..!";
-    }if($no_pages<=0){
-        $message = "No of pages cannot be negative value or 0. re enter the correct value..!";
-    }if (is_float($price+0) && ($price>0)){
-        $message = "--------------------------------";
-    }
-
-    $data = array("id"=>$id,"title"=>$title,"author"=>$author,"ISBN"=>$ISBN,"category_no"=>$category_no,
-        "publisher_name"=>$publisher_name,"published_date"=>$published_date,"publisher_address"=>$publisher_address,"price"=>$price,
-        "no_pages"=>$no_pages,"date_added"=>$date_added,"book_type"=>$book_type,"book_status"=>$book_status,"remarks"=>$remarks,);
-
-    $book->bind($data);
-    $book->insert($dbObj);
-    $message= "Book added successfully..!!";
-
-    echo $id." ".$title." ".$author." ".$ISBN." ".$category_no." ".$publisher_name." ".$published_date." ".$publisher_address." ".$price." ".$no_pages." ".$date_added." ".$book_type." ".$book_status."".$remarks;
-}
-
-$dbObj->closeConnection();
-?>
-
-
