0f8f21f5b0ac5fc213462b88411560e32d17914c
Shalika-Madhushanki <shali.madhushanki@gmail.com>
2017-07-13 19:30:31 +0530

    modified Administration page and tbale page and database page

--- admin/Administration Page.php
+++ admin/Administration Page.php
@@ -18,13 +18,11 @@ $welcomeMsg="";
 	</div>
 	</div>
 	<div class="bgimage">
+        <p class="WelMsg"><?php echo $welcomeMsg;?></p>
 	<nav>
 		<ul>
 
-
-
 			<li><a href="adminDetailSettings.php">Admin Profile</a></li>
-
 
             <?php
             if(!empty($_SESSION['username'])){ ?>
@@ -41,7 +39,6 @@ $welcomeMsg="";
                 $welcomeMsg= " Welcome to Siyane National College of Education Library Administration Page ".$_SESSION['adminName']."...!!!";
             }
             ?>
-            <p class="WelMsg"><?php echo $welcomeMsg;?></p>
 
             <div class="linkbox" id="addbook"><span><strong>Add Book</strong><br /><br /></span>
                 <a href="AddBook.php"><img src="images/addbook.png" align="center"/></a></div><br />
